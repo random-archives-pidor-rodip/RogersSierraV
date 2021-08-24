1927f7651904cd8a5ecf1dc42edb0de5687ca9df
ranstar74 <falconr1125@gmail.com>
2021-08-24 21:54:25 +0300

    Add func that allows to execute action for all component props

--- RogersSierra/Abstract/Component.cs
+++ RogersSierra/Abstract/Component.cs
@@ -1,4 +1,7 @@
-﻿namespace RogersSierra.Abstract
+﻿using FusionLibrary;
+using System;
+
+namespace RogersSierra.Abstract
 {
     /// <summary>
     /// Unity style component system.
@@ -29,5 +32,35 @@
         /// Being called on dispose of train attached to it.
         /// </summary>
         public abstract void Dispose();
+
+        /// <summary>
+        /// Spawns all <see cref="AnimateProp"/> of this <see cref="Component"/>.
+        /// </summary>
+        public void AllProps(Action<AnimateProp> action)
+        {
+            var animatePropType = typeof(AnimateProp);
+
+            // Get all AnimateProp fields of the component
+            var componentType = GetType();
+            var fields = componentType.GetFields();
+
+            // Go through each field and process only AnimateProp ones
+            for (int i = 0; i < fields.Length; i++)
+            {
+                var field = fields[i];
+
+                if (field.FieldType == animatePropType)
+                {
+                    // Get AnimateProp field
+                    var animatePropField = componentType.GetField(field.Name);
+
+                    // Get AnimateProp value
+                    var animatePropValue = (AnimateProp)animatePropField.GetValue(this);
+
+                    action(animatePropValue);
+                    //animatePropValue.SpawnProp();
+                }
+            }
+        }
     }
 }
