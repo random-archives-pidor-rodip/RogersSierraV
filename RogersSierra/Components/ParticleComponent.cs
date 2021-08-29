5070218a1f98b3d637ab761b72b6390772f43d5c
ranstar74 <falconr1125@gmail.com>
2021-08-30 00:20:21 +0300

    Add particle component

---
+++ RogersSierra/Components/ParticleComponent.cs
@@ -0,0 +1,30 @@
+﻿using RogersSierra.Abstract;
+using RogersSierra.Sierra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogersSierra.Components
+{
+    public class ParticleComponent : Component
+    {
+        /// <summary>
+        /// Constructs new instance of <see cref="ParticleComponent"/>.
+        /// </summary>
+        /// <param name="train"></param>
+        public ParticleComponent(Train train) : base(train)
+        {
+
+        }
+
+        public override void OnInit()
+        {
+        }
+
+        public override void OnTick()
+        {
+        }
+    }
+}
