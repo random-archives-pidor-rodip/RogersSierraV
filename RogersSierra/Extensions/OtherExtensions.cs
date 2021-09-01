aaa8c2c691ce91c69b5a08ce6a48b9828fe2b096
bttf92 <lucadigiovanni@outlook.it>
2021-09-01 08:16:39 +0200

    Fixed references + implemented SetState for ParticlePlayer in FusionLibrary

--- RogersSierra/Extensions/OtherExtensions.cs
+++ RogersSierra/Extensions/OtherExtensions.cs
@@ -7,36 +7,6 @@ namespace RogersSierra.Extensions
     /// </summary>
     public static class OtherExtensions
     {
-        /// <summary>
-        /// True: Spawns particle if not spawned already. False: Deletes particle.
-        /// </summary>
-        /// <param name="player">Particle to process.</param>
-        /// <param name="state">Spawn/Delete</param>
-        public static void SetState(this ParticlePlayer player, bool state)
-        {
-            if (state)
-            {
-                if (!player.IsPlaying)
-                    player.Play();
-            }
-            else
-                player.Stop();
-        }
 
-        /// <summary>
-        /// True: Spawns particle if not spawned already. False: Deletes particle.
-        /// </summary>
-        /// <param name="player">Particle to process.</param>
-        /// <param name="state">Spawn/Delete</param>
-        public static void SetState(this ParticlePlayerHandler playerHandler, bool state)
-        {
-            if (state)
-            {
-                if (!playerHandler.IsPlaying)
-                    playerHandler.Play();
-            }
-            else
-                playerHandler.Stop();
-        }
     }
 }
