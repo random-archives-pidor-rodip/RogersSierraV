8be782828a0c4752b569fe6b1c40271c36cee429
ranstar74 <falconr1125@gmail.com>
2021-09-18 23:41:39 +0300

    Detach random props on derail

---
+++ RogersSierra/Components/FunctionalComponents/CollisionComponent.cs
@@ -0,0 +1,34 @@
+﻿using RageComponent;
+using FusionLibrary.Extensions;
+using FusionLibrary;
+using RogersSierra.Other;
+
+namespace RogersSierra.Components.FunctionalComponents
+{
+    /// <summary>
+    /// Handles what to do on collision.
+    /// </summary>
+    public class CollisionComponent : Component<RogersSierra>
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override void Start()
+        {
+            // Detach every prop on collision
+            Base.CustomTrain.CollisionComponent.OnDerail += () =>
+            {
+                var totalProps = Base.TrainComponents.AllHandlerComponentProps.Count;
+                for (int i = 0; i < FusionUtils.Random.Next(5, totalProps); i++)
+                {
+                    var randomProp =
+                        Base.TrainComponents.AllHandlerComponentProps.SelectRandomElement();
+                    randomProp.ScatterProp(FusionUtils.Random.Next(1, 4));
+                }
+
+                Utils.ProcessAllValuesFieldsByType<AnimatePropsHandler>(
+                    Base.WheelComponent, x => x.ScatterProp(3));
+            };
+        }
+    }
+}
